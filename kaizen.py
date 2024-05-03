0b002f4cbb6f1cab9340c0a73ab4407c0a98de43
pedrolopes951 <pedro.miguel.lopes951@gmail.com>
2024-05-03 11:22:46 +0100

    Added some HTTP API to test USER db

--- kaizen.py
+++ kaizen.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from dataModels.dataModels import db
+from flask import Flask,abort
+from dataModels.dataModels import db, User
 
 class KaizenApp:
     def __init__(self) -> None:
@@ -8,7 +8,27 @@ class KaizenApp:
         db.init_app(self.app)
         with self.app.app_context():
             db.create_all()
+    
+    def get_user(self,user_id:int)->int:
+        user = User.query.get(user_id)
+        if user:
+            return user_id
+        else:
+            abort(404,f"User with ID {user_id} not found")
+    
+    def create_user(self,username:str,email:str)->str:
+        if not username or not email:
+            return "Username and email are requiered",400
+        
+        new_user = User()
+        new_user.username = username
+        new_user.email = email
 
 
+        db.session.add(new_user) # Adds  the new_user object to the current session during the next flush operation
+        db.session.commit()# commits th current transation making any pedning changes like inserts updates or deletes permanet in the db
+        return f"User created with Id: {new_user.id}"
+    
     def run(self):
         self.app.run(debug=True)
+    
